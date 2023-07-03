f7ab68a74500bb4373c8564916d971958562286c
abiepabustan <136476978+abiepabustan@users.noreply.github.com>
2023-07-03 23:48:12 +0800

    Implement assignment functions

--- assignment-templates/ipa-3.py
+++ assignment-templates/ipa-3.py
@@ -41,7 +41,55 @@ def relationship_status(from_member, to_member, social_graph):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    from_following = social_graph.get(from_member, {}).get('following', [])
+    to_following = social_graph.get(to_member, {}).get('following', [])
+
+    if to_member in from_following and from_member in to_following:
+        return "friends"
+    elif to_member in from_following:
+        return "follower"
+    elif from_member in to_following:
+        return "followed by"
+    else:
+        return "no relationship"
+
+social_graph = {
+    "@bongolpoc": {
+        "first_name": "Joselito",
+        "last_name": "Olpoc",
+        "following": []
+    },
+    "@joaquin": {
+        "first_name": "Joaquin",
+        "last_name": "Gonzales",
+        "following": ["@chums", "@jobenilagan"]
+    },
+    "@chums": {
+        "first_name": "Matthew",
+        "last_name": "Uy",
+        "following": ["@bongolpoc", "@miketan", "@rudyang", "@joeilagan"]
+    },
+    "@jobenilagan": {
+        "first_name": "Joben",
+        "last_name": "Ilagan",
+        "following": ["@eeebeee", "@joeilagan", "@chums", "@joaquin"]
+    },
+    "@joeilagan": {
+        "first_name": "Joe",
+        "last_name": "Ilagan",
+        "following": ["@eeebeee", "@jobenilagan", "@chums"]
+    },
+    "@eeebeee": {
+        "first_name": "Elizabeth",
+        "last_name": "Ilagan",
+        "following": ["@jobenilagan", "@joeilagan"]
+    },
+}
+
+print(relationship_status("@chums", "@jobenilagan", social_graph))
+print(relationship_status("@joeilagan", "@chums", social_graph))
+print(relationship_status("@joaquin", "@bongolpoc", social_graph))
+print(relationship_status("@bongolpoc", "@chums", social_graph))
 
 
 def tic_tac_toe(board):
@@ -70,7 +118,69 @@ def tic_tac_toe(board):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    size = len(board)
+
+    for row in board:
+        if all(cell == row[0] and cell for cell in row):
+            return row[0]
+
+    for col in range(size):
+        if all(board[row][col] == board[0][col] and board[row][col] for row in range(size)):
+            return board[0][col]
+
+    if all(board[i][i] == board[0][0] and board[i][i] for i in range(size)):
+        return board[0][0]
+
+    if all(board[i][size - 1 - i] == board[0][size - 1] and board[i][size - 1 - i] for i in range(size)):
+        return board[0][size - 1]
+
+    return "NO WINNER"
+
+board1 = [
+    ['X', 'X', 'O'],
+    ['O', 'X', 'O'],
+    ['O', '', 'X'],
+]
+board2 = [
+    ['X', 'X', 'O'],
+    ['O', 'X', 'O'],
+    ['', 'O', 'X'],
+]
+board3 = [
+    ['O', 'X', 'O'],
+    ['', 'O', 'X'],
+    ['X', 'X', 'O'],
+]
+board4 = [
+    ['X', 'X', 'X'],
+    ['O', 'X', 'O'],
+    ['O', '', 'O'],
+]
+board5 = [
+    ['X', 'X', 'O'],
+    ['O', 'X', 'O'],
+    ['X', '', 'O'],
+]
+board6 = [
+    ['X', 'X', 'O'],
+    ['O', 'X', 'O'],
+    ['X', '', ''],
+]
+board7 = [
+    ['X', 'X', 'O', ''],
+    ['O', 'X', 'O', 'O'],
+    ['X', '', '', 'O'],
+    ['O', 'X', '', ''],
+]
+
+print(tic_tac_toe(board1))
+print(tic_tac_toe(board2))
+print(tic_tac_toe(board3))
+print(tic_tac_toe(board4))
+print(tic_tac_toe(board5))
+print(tic_tac_toe(board6))
+print(tic_tac_toe(board7))
+
 
 def eta(first_stop, second_stop, route_map):
     '''ETA.
@@ -103,4 +213,46 @@ def eta(first_stop, second_stop, route_map):
     '''
     # Replace `pass` with your code.
     # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    def calculate_travel_time(route_map, current_stop, target_stop, total_time):
+        if current_stop == target_stop:
+            return total_time
+
+        for leg in route_map:
+            if leg[0] == current_stop:
+                next_stop = leg[1]
+                travel_time = route_map[leg]['travel_time_mins']
+                return calculate_travel_time(route_map, next_stop, target_stop, total_time + travel_time)
+
+        return None
+
+    return calculate_travel_time(route_map, first_stop, second_stop, 0)
+
+legs = {
+    ("upd", "admu"): {
+        "travel_time_mins": 10
+    },
+    ("admu", "dlsu"): {
+        "travel_time_mins": 35
+    },
+    ("dlsu", "upd"): {
+        "travel_time_mins": 55
+    },
+    
+    ('a1', 'a2'): {
+        'travel_time_mins': 10
+    },
+    ('a2', 'b1'): {
+        'travel_time_mins': 10230
+    },
+    ('b1', 'a1'): {
+        'travel_time_mins': 1
+    }
+}
+
+print(eta("upd", "admu", legs))
+print(eta("admu", "dlsu", legs))
+print(eta("dlsu", "upd", legs))
+print(eta("a1", "a2", legs))
+print(eta("a2", "b1", legs))
+print(eta("b1", "a1", legs))
+
